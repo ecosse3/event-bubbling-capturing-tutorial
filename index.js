9ea06a69888180a614996c849a4aca170b115704
ecosse3 <theecosse@gmail.com>
2020-06-18 20:31:05 +0200

    Added translatePhase func

--- index.js
+++ index.js
@@ -6,25 +6,37 @@ document.addEventListener('DOMContentLoaded', () => {
   const five = document.querySelector('.five')
 
   one.addEventListener('bubblingcapturing', event => {
-    console.log(`Otrzymano zdarzenie - el. 1. Faza ${event.eventPhase}`)
+    console.log(`Otrzymano zdarzenie - el. 1. Faza ${translatePhase(event.eventPhase)}`)
   }, {capture: true})
 
   two.addEventListener('bubblingcapturing', event => {
-    console.log(`Otrzymano zdarzenie - el. 2. Faza ${event.eventPhase}`)
+    console.log(`Otrzymano zdarzenie - el. 2. Faza ${translatePhase(event.eventPhase)}`)
   }, {capture: true})
 
   three.addEventListener('bubblingcapturing', event => {
-    console.log(`Otrzymano zdarzenie - el. 3. Faza ${event.eventPhase}`)
+    console.log(`Otrzymano zdarzenie - el. 3. Faza ${translatePhase(event.eventPhase)}`)
   }, {capture: true})
 
   four.addEventListener('bubblingcapturing', event => {
-    console.log(`Otrzymano zdarzenie - el. 4. Faza ${event.eventPhase}`)
+    console.log(`Otrzymano zdarzenie - el. 4. Faza ${translatePhase(event.eventPhase)}`)
   }, {capture: true})
 
   five.addEventListener('bubblingcapturing', event => {
-    console.log(`Otrzymano zdarzenie - el. 5. Faza ${event.eventPhase}`)
+    console.log(`Otrzymano zdarzenie - el. 5. Faza ${translatePhase(event.eventPhase)}`)
   }, {capture: true})
 
   five.dispatchEvent(new CustomEvent('bubblingcapturing'))
 
 })
+
+function translatePhase(eventPhase) {
+  switch (eventPhase) {
+    case 1:
+      return 'capturing';
+    case 2:
+      return 'at target';
+    case 3:
+      return 'bubbling';
+
+  }
+}
